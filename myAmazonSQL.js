a7be76f944bac878765c89d1b51d5ef107b0ff12
megzimo <40956115+megzimo@users.noreply.github.com>
2018-10-22 21:50:08 -0500

    Customer View almost working - need to update db and reprint to finish

--- myAmazonSQL.js
+++ myAmazonSQL.js
@@ -13,8 +13,47 @@ connection.connect(function(err){
     console.log("connected as id: " + connection.threadId);
     // connection checked and is working 10.22.18
     readProducts();
+    welcomePurchase();
+
 })
 
+////////////////////////////////////////////// inquirer: product id and then quanity purchased \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    function welcomePurchase(){
+        inquirer.prompt([{
+            name: "item",
+            type: "input",
+            message: "What item would you like to purchase?",
+        },
+        {
+            name: "amount",
+            type: "input",
+            message: "How many would do you wish to purchase?"   
+        }]).then(function (customerSelect){
+            console.log(customerSelect);
+            let query = connection.query ("SELECT * FROM products WHERE item_id=?", customerSelect.item, function (err, res){
+                if (err) throw err;
+                console.log(res);
+                updateDB();
+            // let query2 = connection.query("UPDATE products SET ? WHERE ?", [
+            //     {
+            //         stock_quantity: res[0].amount - customerSelect.amount
+            //     },
+            //     {
+            //         id: customerSelect.item_id
+            //     }
+            // ]);
+            // console.log("updated quantity: ");
+
+
+                readProducts();
+                if (!res){
+                    console.log("Oops! It looks like you did not select a valid product. Please select an item listen by typing in the corresponding Item ID")
+                };
+            });
+        });
+    };
+
+////////////////////////////////////////////////// Prints store inventory table \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function readProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
@@ -29,18 +68,27 @@ function readProducts() {
         // loop through the response and print out each row into the table
         for (var i = 0; i < res.length; i++) {   
         table.push(
-            [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            [res[i].item_id, res[i].product_name, res[i].department_name, `$`+res[i].price, res[i].stock_quantity]
         );
-    
     }; // ENDS for loop
 
-    console.log(table.toString());
+    console.log("\n" + table.toString());
 
-    }); // ENDS resonse
-
+    }); // ENDS response
   }; // ENDS readProducts()
 
-// inquirer to buy: product id and then quanity
-    // if not enough --> insufficient quanitity
 
-// update table   
+////////////////////////////////////////////////// Update Table \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function updateDB(customerSelect){
+    let customerAmount = customerSelect.amount
+    console.log(customerAmount);
+    connection.query("UPDATE products SET ? WHERE ?", function(err, res) {[
+        {
+            stock_quantity: stock_quantity - customerSelect.amount
+        },
+        {
+            id: customerSelect.item_id
+        }
+    ]});
+    console.log("updated quantity: ", stock_quantity);
+}; // ENDS updateDB()
